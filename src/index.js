33b9f49f38b1ef52c55c326c0942c70cdeedd9d9
KatElkinCodes <kat.elkin.codes@gmail.com>
2025-09-21 13:12:54 +0100

    Fixed apiURL

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let context =
     "You are a romantic poet. Yout mission is to write a 4 line poem in basic html and separate each line with a <br />. Make sure you follow the user instructions. Sign the end of the poem with 'SheCodes AI' inside a <strong> element. Do not include a poem title.";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={apiKey}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
 }
