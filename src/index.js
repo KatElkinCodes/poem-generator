7d51b24c02d3bdb436736160446574ef8908bf0a
KatElkinCodes <kat.elkin.codes@gmail.com>
2025-09-21 13:15:53 +0100

    updated promot

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generatePoem(event) {
   let apiKey = "5a8021b5bae08tb216f43a8oa73a3099";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic poet. Yout mission is to write a 4 line poem in basic html and separate each line with a <br />. Make sure you follow the user instructions. Sign the end of the poem with 'SheCodes AI' inside a <strong> element. Do not include a poem title.";
+    "You are a romantic poet. Write a 4 line poem and separate each line with a <br />. Make sure you follow the user instructions. Sign the end of the poem with SheCodes AI inside a <strong> element. Do not include a poem title.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
